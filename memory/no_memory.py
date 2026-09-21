e1222c91a31e864c8c82c46faaa486c9b6c97b11
Su <2697211288@qq.com>
2026-09-22 00:31:29 +0800

    Clarify no_memory.py memory write behavior

--- memory/no_memory.py
+++ memory/no_memory.py
@@ -6,8 +6,8 @@ from .base import BaseMemory, MemoryItem
 class NoMemory(BaseMemory):
     name = "no_memory"
 
-    def add(self, item: MemoryItem) -> None:
-        return None
+    def add(self, item: MemoryItem) -> bool:
+        return False
 
     def retrieve(self, query: str, top_k: int = 3) -> list[MemoryItem]:
         return []
